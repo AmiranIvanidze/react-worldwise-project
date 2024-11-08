67b69d5fd1128eb78aef4db44cf83065505ad9e4
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-08 22:14:44 +0400

    Use URL and Routes for state managment

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000"
 const App = () => {
@@ -40,6 +41,7 @@ const App = () => {
             <Route path="app" element={ <AppLayout/> } >
               <Route index element={<p>List Of Cities</p>}/>
               <Route path="cities" element={ <CityList cities={cities} isLoading={isLoading} /> }/>
+              <Route path="cities/:id" element={ <City /> }></Route>
               <Route path="countries" element={ <CountryList cities={cities} isLoading={isLoading} /> }/>
               <Route path="form" element={ <p>List OF Forms</p> }/>
             </Route>
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css'
 
 const formatDate = (date) =>
@@ -10,14 +11,16 @@ const formatDate = (date) =>
     }).format(new Date(date));
 
 const CityItem = ({ city }) => {
-    const { cityName, emoji, date } = city;
+    const { cityName, emoji, date, id } = city;
     return (
-            <li className={styles.cityItem}>
-                <span className={styles.emoji}>
-                    {emoji}
-                </span>
-                <h3 className={styles.name}>{cityName}</h3>
-                <time className={styles.date}>{formatDate(date)}</time>
+            <li>
+                <Link className={styles.cityItem} to={`${id}`}>
+                    <span className={styles.emoji}>
+                        {emoji}
+                    </span>
+                    <h3 className={styles.name}>{cityName}</h3>
+                    <time className={styles.date}>{formatDate(date)}</time>
+                </Link>
             </li>
         )
     }
