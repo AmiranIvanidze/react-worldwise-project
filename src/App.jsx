58ea2727ea148ac211ea4f508bfb8f2950feba5e
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-08 19:22:38 +0400

    Implement The Countries List Using Json-server

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,30 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000"
 const App = () => {
+
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function(){
+    async function fetchCities(){
+      try{
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      }catch {
+        console.error("Error occured in cities fetch")
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  }, []);
   return (
     <BrowserRouter>
         <Routes>
@@ -15,7 +38,7 @@ const App = () => {
             
             <Route path="app" element={ <AppLayout/> } >
               <Route index element={<p>List Of Cities</p>}/>
-              <Route path="cities" element={ <p>List OF Cities</p> }/>
+              <Route path="cities" element={ <CityList cities={cities} isLoading={isLoading} /> }/>
               <Route path="countries" element={ <p>List OF Countries</p> }/>
               <Route path="form" element={ <p>List OF Forms</p> }/>
             </Route>
