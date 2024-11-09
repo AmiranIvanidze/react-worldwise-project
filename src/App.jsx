237c09018e745a20f46bc9067e065c21f6e4cdab
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-09 20:43:37 +0400

    Add redirect from AppLayout index route to cities route and import Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
@@ -40,7 +40,7 @@ const App = () => {
             <Route path="pricing" element={ <Pricing/> }/>
             
             <Route path="app" element={ <AppLayout/> } >
-              <Route index element={<p>List Of Cities</p>}/>
+              <Route index element={<Navigate replace to="cities"/>}/>
               <Route path="cities" element={ <CityList cities={cities} isLoading={isLoading} /> }/>
               <Route path="cities/:id" element={ <City /> }></Route>
               <Route path="countries" element={ <CountryList cities={cities} isLoading={isLoading} /> }/>
