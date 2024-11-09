86b8881820d397d18cfe7d24f3318f200a603d64
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-09 15:38:09 +0400

    Add navigation on Map component to redirect to Form route on map click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,13 @@
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 const Map = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   
   const lng = searchParams.get('lng');
   const lat = searchParams.get('lat');
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => {navigate('form')}}>
         <h1>Map</h1>
         <h1>Position: {lat}, {lng}</h1>
         <button onClick={() => setSearchParams({lat:23, lng:50})}>Change position</button>
