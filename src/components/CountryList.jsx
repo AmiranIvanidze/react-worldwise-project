dfc9e52607997944ef4dfc5a67a13672799ecef2
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-11 16:12:36 +0400

    Refactor CountryList to use cities context for data and loading state

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,10 @@ import styles from './CountryList.module.css';
 import Spinner from './Spinner';
 import Message from './Message';
 import CountryItem from './CountryItem';
+import { useCities } from '../contexts/CitiesContext';
 
-const CountryList = ({ cities, isLoading }) => {
+const CountryList = () => {
+    const { cities, isLoading } = useCities();
     if(isLoading) return <Spinner />
     if(!cities.length) return <Message message="Add your first city by clicking on a city on the map" />
 
