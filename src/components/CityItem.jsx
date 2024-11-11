fbda05d6f6b8ca9e14344d30fb81f3c7d03217af
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-11 18:04:31 +0400

    Integrate useCities context in CityItem and add active state styling for the current city.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 
 import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css'
+import { useCities } from '../contexts/CitiesContext';
 
 const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -11,10 +12,12 @@ const formatDate = (date) =>
     }).format(new Date(date));
 
 const CityItem = ({ city }) => {
+    const {currentCity} = useCities(); 
     const { cityName, emoji, date, id, position } = city;
+
     return (
             <li>
-                <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+                <Link className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active'] : '' }`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
                     <span className={styles.emoji}>
                         {emoji}
                     </span>
