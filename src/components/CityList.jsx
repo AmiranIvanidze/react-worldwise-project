b2ce791084892a993cbf8217b7dd381eac78a0d5
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-11 16:12:23 +0400

    Refactor CityList to use cities context for data and loading state

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,10 +2,12 @@ import CityItem from './CityItem';
 import styles from './CityList.module.css';
 import Spinner from './Spinner';
 import Message from './Message';
+import { useCities } from '../contexts/CitiesContext';
 
-const CityList = ({ cities, isLoading }) => {
+const CityList = () => {
+  const { cities, isLoading } = useCities();
     if(isLoading) return <Spinner />
-    if(!cities.length) return <Message message="Add your first city by clicking on a city on the map" />
+    if(!cities?.length) return <Message message="Add your first city by clicking on a city on the map" />
   return (
 
     <ul className={styles.cityList}>
