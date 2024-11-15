b39c3f0744b1c5b60e51cc3ae7f9e3d604d05c74
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-15 16:52:47 +0400

    Implement If User Is Authenticated In AppLayout.jsx

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react"
 import Map from "../components/Map"
 import SideBar from "../components/SideBar"
+import User from "../components/User"
+import { useAuth } from "../contexts/FakeAuthContext"
 import styles from './AppLayout.module.css'
+import { useNavigate } from "react-router-dom"
 const AppLayout = () => {
+  const {user, isAuthenticated, login, logout} = useAuth();
+  const navigate = useNavigate();
+
+    useEffect(function(){
+      if(!isAuthenticated){
+        navigate('/')
+      }
+    },[isAuthenticated])
+ 
   return (
     <div className={styles.app}> 
        <SideBar />
-       <Map />
+       <Map/>
+       {isAuthenticated && (
+        <User logout={logout} />
+       )}
     </div>
   )
 }
